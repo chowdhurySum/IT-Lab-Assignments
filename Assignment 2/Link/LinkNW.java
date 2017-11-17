60139ffbd96328b3b3f8f8a4f6a68535d478d4f5
Sumitra Chowdhury <chowdhury9995@gmail.com>
2017-11-18 02:51:34 +0530

    Add assignment 8
    open a link in a new window using Applet

---
+++ Assignment 2/Link/LinkNW.java
@@ -0,0 +1,39 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+import java.net.*;
+
+public class Link extends Applet implements ActionListener
+{
+	Button b;
+	TextField txtUrl;
+	//String str = new String("Enter website here");
+	public void init () {
+		txtUrl = new TextField(30);
+		add(txtUrl);
+		b = new Button("go");
+		b.addActionListener(this);
+		add(b);
+	}
+	
+	public void actionPerformed(ActionEvent ae) {
+		Button source = (Button)ae.getSource();
+		if(source == b){
+			//Desktop d = Desktop.getDesktop();
+			String link = "https://www." + txtUrl.getText() + ".com";
+			//System.out.println(link);
+			
+			
+			try {
+				//d.browse(new URI(link));
+				AppletContext ac = getAppletContext();
+				URL url = new URL(link);
+				ac.showDocument(url,"_blank");//show a new window
+			}
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+			
+		}
+	}	
+}
